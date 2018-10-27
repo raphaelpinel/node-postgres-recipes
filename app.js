96b398cf5748f7139ddf44b663066a55ffb0fcde
Raphael Pinel <raphaelpinel8@gmail.com>
2018-10-27 17:53:07 +0200

    defined variables

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require('express'),
+      path = require('path'),
+      bodyParser = require('body-parser'),
+      cons = require('consolidate'),
+      dust = require('dustjs-helpers'),
+      pg = require('pg'),
+      app = express();
