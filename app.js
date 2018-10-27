6f444581063b100c5fd0ff6815249eada63e9e20
Raphael Pinel <raphaelpinel8@gmail.com>
2018-10-27 21:19:19 +0200

    connect to database and display recipe name

--- app.js
+++ app.js
@@ -5,24 +5,37 @@ const express = require('express'),
       dust = require('dustjs-helpers'),
       pg = require('pg'),
       app = express();
+      
+      //Assign Dust Engine To .dust Files
+      app.engine('dust', cons.dust);
+      
+      // Set .dust as default extension
+      app.set('view engine', 'dust');
+      app.set('views', __dirname + '/views');
+      
+      // Set public folder
+      app.use(express.static(path.join(__dirname, 'public')));
+      
+      //Body Parser Middleware
+      app.use(bodyParser.json());
+      app.use(bodyParser.urlencoded({ extended: false }));
+      
+      //Route
+      app.get('/', function(req, res){
+          //res.render('index');   
+          // PG connect
+          const { Pool, Client } = require('pg')
+          const connectionString = "postgres://recipe:success@localhost/recipebookdb";
 
-//Assign Dust Engine To .dust Files
-app.engine('dust', cons.dust);
+          const pool = new Pool({
+            connectionString: connectionString,
+          })
 
-// Set .dust as default extension
-app.set('view engine', 'dust');
-app.set('views', __dirname + '/views');
-
-// Set public folder
-app.use(express.static(path.join(__dirname, 'public')));
-
-//Body Parser Middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-//Route
-app.get('/', function(req, res){
-    res.render('index');    
+    pool.query('SELECT * FROM recipes', (err, result) => {
+    console.log(err, result);
+    res.render('index', {recipes: result.rows});
+    pool.end();
+    }) 
 });
 //Server
 app.listen(3000, function (){ 
